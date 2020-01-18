8538184d5cb1de13ad872142a3ac3bffa2bb3165
Matthew Lowe <lowe.matthew@mail.com>
2020-01-18 21:49:19 +0000

    Launch biscuit from under the cursor

--- Assets/Scripts/Biscuit.cs
+++ Assets/Scripts/Biscuit.cs
@@ -5,10 +5,9 @@ using UnityEngine.EventSystems;
 
 public class Biscuit : MonoBehaviour
 {
-    [SerializeField] private BiscuitData Scales;
+    [SerializeField] private BiscuitData LaunchData;
 
-    private Vector3 mDelta;
-    private Vector3 mLastMousePos;
+    private Plane mLaunchPlane;
     private Vector3 mLastVel;
 
     public bool Thrown { get; private set; }
@@ -16,25 +15,25 @@ public class Biscuit : MonoBehaviour
     void Start()
     {
         Thrown = false;
+        mLaunchPlane = new Plane(transform.up, transform.position);
         GetComponent<Rigidbody>().isKinematic = true;
-    }
-
-    void Update()
-    {
-        mDelta = Input.mousePosition - mLastMousePos;
-        mLastMousePos = Input.mousePosition;
     }
 
     public void OnMouseDrag()
     {
-        mLastVel = Scales.DragScale * (transform.forward * mDelta.y + transform.right * mDelta.x);
-        transform.position += mLastVel;
+        var lastPos = transform.position;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float distToPlane;
+
+        mLaunchPlane.Raycast(ray, out distToPlane);
+        transform.position = ray.GetPoint(distToPlane);
+        mLastVel = transform.position - lastPos;
     }
 
     private void OnMouseUp()
     {
         Thrown = true;
         GetComponent<Rigidbody>().isKinematic = false;
-        GetComponent<Rigidbody>().velocity = mLastVel * Scales.LaunchScale;
+        GetComponent<Rigidbody>().velocity = mLastVel * LaunchData.LaunchScale;
     }
 }
--- Assets/Scripts/BiscuitData.cs
+++ Assets/Scripts/BiscuitData.cs
@@ -5,19 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/BiscuitData", order = 1)]
 public class BiscuitData : ScriptableObject
 {
-    [SerializeField] private float DragScaleDesktop = 0.01f;
     [SerializeField] private float LaunchScaleDesktop = 20.0f;
-    
-    [SerializeField] private float DragScaleApp = 0.001f;
     [SerializeField] private float LaunchScaleApp = 34.0f;
-
     [SerializeField] private bool UseDesktop = true;
-
-    public float DragScale {
-        get {
-            return UseDesktop ? DragScaleDesktop : DragScaleApp;
-        }
-    }
 
     public float LaunchScale {
         get {
