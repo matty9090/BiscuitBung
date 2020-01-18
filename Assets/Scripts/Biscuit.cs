15235dfdc11069d4a4de7fb2e41a6251a9a23f23
Matthew Lowe <lowe.matthew@mail.com>
2020-01-18 20:14:31 +0000

    Basic throwing mechanics

---
+++ Assets/Scripts/Biscuit.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Biscuit : MonoBehaviour
+{
+    [SerializeField] private float DragScale = 0.01f;
+    [SerializeField] private float ThrowScale = 4.0f;
+
+    private Vector3 mDelta;
+    private Vector3 mLastMousePos;
+    private Vector3 mLastVel;
+
+    public bool Thrown { get; private set; }
+
+    void Start()
+    {
+        Thrown = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    void Update()
+    {
+        mDelta = Input.mousePosition - mLastMousePos;
+        mLastMousePos = Input.mousePosition;
+    }
+
+    public void OnMouseDrag()
+    {
+        mLastVel = DragScale * (transform.forward * mDelta.y + transform.right * mDelta.x);
+        transform.position += mLastVel;
+    }
+
+    private void OnMouseUp()
+    {
+        Thrown = true;
+        GetComponent<Rigidbody>().isKinematic = false;
+        GetComponent<Rigidbody>().velocity = mLastVel * ThrowScale;
+    }
+}
---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    [SerializeField] private GameObject BiscuitType = null;
+    private GameObject Biscuit = null;
+
+    void Start()
+    {
+        Biscuit = Instantiate(BiscuitType, transform.position, transform.rotation);
+    }
+
+    void Update()
+    {
+        if (Biscuit.GetComponent<Biscuit>().Thrown &&
+            Biscuit.GetComponent<Rigidbody>().velocity.magnitude <= 0.001f)
+        {
+            Debug.Log("Spawning");
+            Destroy(Biscuit);
+            Biscuit = Instantiate(BiscuitType, transform.position, transform.rotation);
+        }
+    }
+}
