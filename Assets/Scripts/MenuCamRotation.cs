96526325b125b8aa181b2f2ba4918f086985704c
Matthew Lowe <lowe.matthew@mail.com>
2020-01-19 14:30:58 +0000

    Add menu screen

---
+++ Assets/Scripts/MenuCamRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCamRotation : MonoBehaviour
+{
+    [SerializeField] private float Speed = 1.0f;
+
+    void Update()
+    {
+        transform.rotation *= Quaternion.Euler(0.0f, Speed * Time.deltaTime, 0.0f);
+    }
+}
